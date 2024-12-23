4bee53e92bf2fb62b699c6b3ccfa367a312c4f74
prashant <prashant.singh.12312345@gmail.com>
2024-12-23 18:05:38 +0530

    first commit

--- lib/utils/app_assets.dart
+++ lib/utils/app_assets.dart
@@ -1,7 +1,9 @@
 class MyAppAssets{
   // PNG Assets
-  static String png_image_ = '';
+  static String png_image_icon = 'assets/png_assets/icon.png';
+  static String png_image_icon_only = 'assets/png_assets/icon_only.png';
 
   // Svg Assets
-  static String svg_image_ = '';
+  static String svg_image_icon_full = 'assets/svg_assets/icon_full.svg';
+  static String svg_image_only_icon = 'assets/svg_assets/only_icon.svg';
 }
